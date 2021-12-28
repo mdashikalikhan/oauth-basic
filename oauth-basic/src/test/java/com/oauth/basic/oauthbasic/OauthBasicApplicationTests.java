a9c893527fed1c45ac8e32de149d9da2f623aa43
ashik.khan <ashik.khan@N1084.sonaliintellect.com>
2021-12-28 20:29:21 +0600

    oauth basic

---
+++ oauth-basic/src/test/java/com/oauth/basic/oauthbasic/OauthBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth.basic.oauthbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
